dba49bce834f28c274f0aee42bd3783341e2cf0e
Debadrita438 <bristibandyopadhyay@gmail.com>
2020-01-26 10:58:39 +0530

    Updates

---
+++ precache-manifest.7d43f037d05b62900f16872baa78c453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58f7f6b8ed91846935642a60f962e82",
+    "url": "/Face-Detection-Brain/index.html"
+  },
+  {
+    "revision": "465cc6f8c3e527ddd481",
+    "url": "/Face-Detection-Brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d47e8a8c52858300fb31",
+    "url": "/Face-Detection-Brain/static/css/main.15293de9.chunk.css"
+  },
+  {
+    "revision": "465cc6f8c3e527ddd481",
+    "url": "/Face-Detection-Brain/static/js/2.324b2922.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Face-Detection-Brain/static/js/2.324b2922.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d47e8a8c52858300fb31",
+    "url": "/Face-Detection-Brain/static/js/main.40fbab87.chunk.js"
+  },
+  {
+    "revision": "4c9dacaaec0e042ea79a",
+    "url": "/Face-Detection-Brain/static/js/runtime-main.952321ce.js"
+  }
+]);
